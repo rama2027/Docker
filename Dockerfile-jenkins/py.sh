0a09aa469dc6715582a5ffe890e6ed8d8d8de040
rama2027 <37704469+rama2027@users.noreply.github.com>
2020-07-20 00:34:39 +0530

    Update py.sh

--- Dockerfile-jenkins/py.sh
+++ Dockerfile-jenkins/py.sh
@@ -2,8 +2,6 @@ yum install sudo -y
 sudo yum install python3 -y
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-pip install virtualenv
-virtualenv mypython
 source $WORKSPACE/mypython/bin/activate
 pip install pip --upgrade
 pip install boto3
