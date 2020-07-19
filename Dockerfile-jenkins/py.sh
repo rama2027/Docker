d36eac0c430e3af7f01906d74ad7803e6e0dddde
rama2027 <37704469+rama2027@users.noreply.github.com>
2020-07-19 23:30:58 +0530

    Update py.sh

--- Dockerfile-jenkins/py.sh
+++ Dockerfile-jenkins/py.sh
@@ -1,6 +1,11 @@
  yum install sudo -y
 sudo yum install python3 -y
-python3 -m venv my_app/env
-source /my_app/env/bin/activate
+python3 pip -h
+pip -h
+python3 pip install virtualenv
+pip install virtualenv
+python3 -m virtualenv mypython
+virtualenv mypython
+source /mypython/bin/activate
 pip install pip --upgrade
 pip install boto3
